e36a900c6056e384f85fc6f43a1800f1ade737f5
ELIJAHKUNGU <elijahkungu100@gmail.com>
2022-08-20 11:33:34 +0300

    frontend

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Tobar from "../../componets/topbar/tobar";
+
+const Home = () => {
+  return (
+    <div>
+      <Tobar />
+    </div>
+  );
+};
+
+export default Home;
